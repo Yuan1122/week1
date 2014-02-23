b7f781624f9cfb463a09ed2384db430569d58de5
Yuan1122 <loki61007@hotmail.com>
2014-02-23 19:26:29 +0800

    week1

---
+++ hw1.java
@@ -0,0 +1,9 @@
+class hw1 { 
+ public static void main(String[] args) {
+	System.out.print("過華清宮\t杜牧\n\n");
+	System.out.print("長安回望繡成堆\n");
+	System.out.print("山頂千門次第開\n");
+	System.out.print("一騎紅塵妃子笑\n");
+	System.out.print("無人知是荔枝來\n\n");	
+	} 
+}
